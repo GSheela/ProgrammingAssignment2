8bc75d64f7196bdcde00e9cea0417e1cf09756c2
GSheela <gsheela@gatech.edu>
2015-07-24 15:29:37 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##makeCacheMatrix function takes a matrix as an input value and returns an inverse matrix as an output
+##This function also stores the matrix and inverse matrix in cache
+##Input for this function can be a variable that stored square invertible matrix or a matrix can be passed directly
+## Example: p <- matrix(c(1,-.25,-.25,3), nrow = 2, ncol = 2) makeCacheMatrix(p)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <<-x
+  invmat <<- solve(x)
+invmat
+    
 }
 
 
-## Write a short comment describing this function
+## cacheSolve matrix takes matrix as an input and verifies whether inverse was already calculated for it
+## If inverse is already calculated, then inverse matrix is retrieved from the cache else it is calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(all(x==mat) )
+  {
+    print("getting cached data")
+    return(invmat)
+  }
+  else
+  {
+    return(solve(x))
+  }
+    
 }
