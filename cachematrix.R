1080f482dcde8e057e844edd648d35b0ca0f970c
GSheela <gsheela@gatech.edu>
2015-07-24 15:43:23 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ##makeCacheMatrix function takes a matrix as an input value and returns an inverse matrix as an output
 ##This function also stores the matrix and inverse matrix in cache
 ##Input for this function can be a variable that stored square invertible matrix or a matrix can be passed directly
-## Example: p <- matrix(c(1,-.25,-.25,3), nrow = 2, ncol = 2) makeCacheMatrix(p)
+##Example: p <- matrix(c(1,-.25,-.25,3), nrow = 2, ncol = 2) makeCacheMatrix(p)
 
 makeCacheMatrix <- function(x = matrix()) {
   mat <<-x
